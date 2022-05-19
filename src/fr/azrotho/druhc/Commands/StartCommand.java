0da7067c2bdd80828d3868f37fe3d0fd6e94847c
Azrotho <48129165+Azrotho@users.noreply.github.com>
2022-05-19 21:27:55 +0200

    add break;

--- src/fr/azrotho/druhc/Commands/StartCommand.java
+++ src/fr/azrotho/druhc/Commands/StartCommand.java
@@ -50,70 +50,87 @@ public class StartCommand implements CommandExecutor {
                 Main.getRoles().put(uuid, "Makoto");
                 Main.getRolesreverse().put("Makoto", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 1:
                 Main.getRoles().put(uuid, "Monokuma");
                 Main.getRolesreverse().put("Monokuma", uuid);
                 Main.getCamp().put(uuid, "Mechant");
+                break;
             case 2:
                 Main.getRoles().put(uuid, "Chihiro");
                 Main.getRolesreverse().put("Chihiro", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 3:
                 Main.getRoles().put(uuid, "Byakuya");
                 Main.getRolesreverse().put("Byakuya", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 4:
                 Main.getRoles().put(uuid, "Junko");
                 Main.getRolesreverse().put("Junko", uuid);
                 Main.getCamp().put(uuid, "Mechant");
+                break;
             case 5:
                 Main.getRoles().put(uuid, "Toko");
                 Main.getRolesreverse().put("Toko", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 6:
                 Main.getRoles().put(uuid, "Mondo");
                 Main.getRolesreverse().put("Mondo", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 7:
                 Main.getRoles().put(uuid, "Sakura");
                 Main.getRolesreverse().put("Sakura", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 8:
                 Main.getRoles().put(uuid, "Aoi");
                 Main.getRolesreverse().put("Aoi", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 9:
                 Main.getRoles().put(uuid, "Hifumi");
                 Main.getRolesreverse().put("Hifumi", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 10:
                 Main.getRoles().put(uuid, "Sayaka");
                 Main.getRolesreverse().put("Sayaka", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 11:
                 Main.getRoles().put(uuid, "Kyoko");
                 Main.getRolesreverse().put("Kyoko", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 12:
                 Main.getRoles().put(uuid, "Celestia");
                 Main.getRolesreverse().put("Celestia", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 13:
                 Main.getRoles().put(uuid, "Mukuro");
                 Main.getRolesreverse().put("Mukoro", uuid);
                 Main.getCamp().put(uuid, "Solo");
+                break;
             case 14:
                 Main.getRoles().put(uuid, "Ishimaru");
                 Main.getRolesreverse().put("Ishimaru", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 15:
                 Main.getRoles().put(uuid, "Yasuhiro");
                 Main.getRolesreverse().put("Yasuhiro", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
             case 16:
                 Main.getRoles().put(uuid, "Leon");
                 Main.getRolesreverse().put("Leon", uuid);
                 Main.getCamp().put(uuid, "Gentil");
+                break;
         }
     }
 
@@ -122,56 +139,76 @@ public class StartCommand implements CommandExecutor {
             case 0:
                 p.sendMessage("§b§lVous êtes Makoto Naegi, Ultime Chanceux, Vous devez éliminer Monokuma, Vous êtes connu de tous, Vous incarnez l'espoir au sein de Kibogamine, Vous devez gagner avec les autres étudients!");
                 Bukkit.broadcastMessage("§c§lL'espoir au sein de cet école, aka Makoto Naegi est: §f§l" + p.getDisplayName());
+                break;
             case 1:
                 p.sendMessage("§c§lVous êtes Monokuma, upupupu, Vous avez comme but d'exterminer les vilains élèves de Kibogamine, Si vous mourrez vous réapparaissez avec un coeur en Moins, Mais si Junko meurt, vous mourrez, à savoir que vous pouvez infecter un Joueur avec §6§l/dr infect <pseudo>");
+                break;
             case 2:
                 p.sendMessage("§b§lVous êtes Chihiro, Ultime Programmeur une frêle fille, Vous devez éliminer Monokuma, mais vous êtes invisible durant les phases de nuit.");
+                break;
             case 3:
                 p.sendMessage("§b§lVous êtes Byakuya, Ultime Héritier, Vous devez éliminer Monokuma, vous possédez Speed II et vous avez l'information quand §bToko §b§l se transforme en §5Genocider Sho");
+                break;
             case 4:
                 p.sendMessage("§c§lVous êtes Junko, Ultime Fashionsta, Vous devez tuer les étudients, vous possédez Force I, et si vous mourrez Monokuma meurt avec vous.");
                 UUID MonoUUID = Main.getRolesreverse().get("Monokuma");
                 Player Monokuma = Bukkit.getPlayer(MonoUUID);
                 p.sendMessage("§a§lPseudo de Monokuma: §f" + Monokuma.getDisplayName());
+                break;
             case 5:
                 p.sendMessage("§b§lVous êtes Toko, Ultime Romancière, Vous devez éliminer Monokuma, Vous n'avez aucun spécial dans la partie, sauf que si vous mourrez vous devenez §5Genocider Sho");
                 UUID ByakuUUID = Main.getRolesreverse().get("Byakuya");
                 Player Byakuya = Bukkit.getPlayer(ByakuUUID);
                 p.sendMessage("§a§lPseudo de Byakuya: §f" + Byakuya.getDisplayName());
+                break;
             case 6:
                 p.sendMessage("§b§lVous êtes Mondo, Ultime Chef de Gang, Vous devez éliminer Monokuma, Vous disposez de l'effet Force.");
+                break;
             case 7:
                 p.sendMessage("§b§lVous êtes Sakura, Ultime Combattante, Vous devez éliminer Monokuma, Vous disposez de l'effet Force et vous connaissez Aoi");
                 UUID AoUUID = Main.getRolesreverse().get("Aoi");
                 Player Aoi = Bukkit.getPlayer(AoUUID);
+<<<<<<< Updated upstream
                 // Pseudo Aoi
+=======
+                break;
+>>>>>>> Stashed changes
             case 8:
                 p.sendMessage("§b§lVous êtes Aoi, Ultime Nageuse, Vous devez éliminer Monokuma, Vous disposez de l'effet Speed I et un Livre Depth Strider 3, Vous connaissez Sakura");
                 UUID SakuUUID = Main.getRolesreverse().get("Sakura");
                 Player Sakura = Bukkit.getPlayer(SakuUUID);
                 p.sendMessage("§a§lPseudo de Sakura: §f" + Sakura.getDisplayName());
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "give " + p.getName() + " minecraft:enchanted_book 1 0 {ench:[{id:8,lvl:3}]}");
+                break;
             case 9:
                 p.sendMessage("§b§lVous êtes Hifumi, Ultime Auteur de Fanzine (vive la 2d), Vous devez éliminer Monokuma, Vous disposez de l'effet Résistance");
+                break;
             case 10:
                 p.sendMessage("§b§lVous êtes Sayaka, Ultime Idol, Vous devez éliminer Monokuma, Vous disposez de l'effet Speed I et d'une Épée Knockback 2");
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "give " + p.getName() + " minecraft:iron_sword 1 0 {ench:[{id:19,lvl:2}]}");
+                break;
             case 11:
                 p.sendMessage("§b§lVous êtes Kyoko, Ultime ???, Vous devez éliminer Monokuma, Vous savez quand un Joueur a été infecté");
+                break;
             case 12:
                 p.sendMessage("§b§lVous êtes Celestia, Ultime Parieuse, Vous devez éliminer Monokuma, Vous avez 15 Coeurs.");
                 p.setMaxHealth(30);
+                break;
             case 13:
                 p.sendMessage("§5§lVous êtes Mukuro, Vous devez gagner seul, Vous avez un Arc Power III et Punch II, Force I et 15 Coeurs");
                 p.setMaxHealth(30);
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "give " + p.getName() + " minecraft:bow 1 0 {ench:[{id:48,lvl:3},{id:49,lvl:2}]}");
+                break;
             case 14:
                 p.sendMessage("§b§lVous êtes Ishimaru, Vous devez éliminer Monokuma, Vous disposez de l'effet Speed II");
+                break;
             case 15:
                 p.sendMessage("§b§lVous êtes Yasuhiro, Ultime Voyant, Vous devez éliminer Monokuma, Avec §6§l/dr check <pseudo1> <pseudo2> <pseudo3>§b§l, Si les joueurs sont dans le même camp");
+                break;
             case 16:
                 p.sendMessage("Vous êtes Leon, Vous devez éliminer Monokuma, Et vous avez un Arc Punch II");
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "give " + p.getName() + " minecraft:bow 1 0 {ench:[{id:49,lvl:2}]}");
+                break;
         }
 
     }
